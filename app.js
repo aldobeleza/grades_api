3087cd9357b9683d33fc7f12608b993f99d024a7
aldo.beleza <aldobeluco@gmail.com>
2020-09-06 21:43:27 -0300

    aaa

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
   cors({
-    origin: 'https://backenddesafio.herokuapp.com/',
+    origin: 'https://frontenddesafio.herokuapp.com/',
   })
 );
 
